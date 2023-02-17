cabed3578ff2ee448f8162802bc2c9ab506b45c5
kikoDevv <103362419+kikoDevv@users.noreply.github.com>
2023-02-17 21:37:28 +0100

    Initial Commit

---
+++ backEndDemo/backEndDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  backEndDemoApp.swift
+//  backEndDemo
+//
+//  Created by Kiko on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct backEndDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
